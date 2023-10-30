3aec21de8384267964bbbf32cadafe1b418602ff
jtan573 <jtan573@e.ntu.edu.sg>
2023-10-30 20:52:06 +0100

    Update "Add New Task" element and Delete Task elements

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.todolist
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -24,8 +33,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todolist.ui.theme.TodoListTheme
+import androidx.compose.material.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,53 +69,75 @@ fun MainScreen (list: MutableList<Task>, modifier: Modifier = Modifier) {
     }
 }
 
+// Add New Task
 @Composable
 fun TextInputView(list: MutableList<Task>) {
-    var text by rememberSaveable {
-        mutableStateOf(value = "")
-    }
-    Row(
-        modifier = Modifier.fillMaxWidth()
+    var titleInput by rememberSaveable { mutableStateOf(value = "") }
+    var descInput by rememberSaveable { mutableStateOf(value = "") }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
     ) {
-        OutlinedTextField(value = text, onValueChange = {
-            text = it
-        })
+        Text(text = "ADD NEW TASK")
+        OutlinedTextField(
+            value = titleInput,
+            onValueChange = { titleInput = it },
+            label = { Text("Title") }
+        )
+        OutlinedTextField(
+            value = descInput,
+            onValueChange = { descInput = it },
+            label = { Text(text = "Description")}
+        )
         Button(onClick = {
-            list.add(Task(title = text))
-            text = ""
+            list.add(Task(title = titleInput, description = descInput))
+            titleInput = ""
+            descInput = ""
         }) {
-            Text(text = "Add")
+            Text(text = "ADD TO LIST")
         }
     }
 }
 
 @Composable
-fun ListView(list: List<Task>) {
+fun ListView(list: MutableList<Task>) {
     LazyColumn {
         items(list) { task ->
-            RowView(task)
+            RowView(task, list)
         }
     }
 }
 
 @Composable
-fun RowView(task: Task) {
-    Row (modifier = Modifier
-        .fillMaxWidth(),
+fun RowView(task: Task, list: MutableList<Task>) {
+    Row (modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
-        )
+    )
     {
         Checkbox(checked = task.isChecked.value,
             onCheckedChange = {task.isChecked.value = !task.isChecked.value}
         )
-        Text(text = task.title)
+        Column {
+            Text(text = task.title, fontWeight = FontWeight.Bold)
+            Text(text = task.description)
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            modifier = Modifier.absolutePadding(right = 5.dp),
+            onClick = { list.removeAt(list.indexOfFirst { it.title == task.title }) },
+            shape = CircleShape
+        ) {
+            Icons.Default.Delete
+        }
     }
 }
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun RowViewPreview() {
     TodoListTheme {
-        RowView(Task(title = "Hello"))
+        RowView(Task(title = "Hello", description = "World"))
     }
 }
+*/
